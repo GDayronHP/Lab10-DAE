4e8386f48fe3ec2b3e6c5552d2af1c793a29bb1d
GDayronHP <geometryhuayra@gmail.com>
2024-10-23 16:07:31 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articulos: [
+        { codigo: 1, descripcion: 'coca-cola', precio: 2.50 },
+        { codigo: 2, descripcion: 'inka-cola', precio: 2.20 },
+        { codigo: 3, descripcion: 'fanta', precio: 1.70 }
+      ]
+    };
+    this.borrar = this.borrar.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="bg-dark text-light min-vh-100 d-flex flex-column justify-content-center align-items-center">
+        <div className="container py-5">
+          <h2 className="text-center mb-4">Lista de Artículos</h2>
+          <div className="table-responsive">
+            <table className="table table-striped table-hover table-bordered">
+              <thead className="thead-dark">
+                <tr className="text-center">
+                  <th>Código</th>
+                  <th>Descripción</th>
+                  <th>Precio</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.articulos.map(elemento => (
+                  <tr key={elemento.codigo} className="text-center">
+                    <td>{elemento.codigo}</td>
+                    <td>{elemento.descripcion}</td>
+                    <td>${elemento.precio.toFixed(2)}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => this.borrar(elemento.codigo)}
+                      >
+                        Borrar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  borrar(cod) {
+    const temp = this.state.articulos.filter(el => el.codigo !== cod);
+    this.setState({
+      articulos: temp
+    });
+  }
 }
 
 export default App;
