b4ef14325428b0f8125c813499242a04c9f207cf
GDayronHP <geometryhuayra@gmail.com>
2024-10-23 16:22:51 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       articulos: [
-        { codigo: 1, descripcion: 'coca-cola', precio: 2.50 },
-        { codigo: 2, descripcion: 'inka-cola', precio: 2.20 },
-        { codigo: 3, descripcion: 'fanta', precio: 1.70 }
-      ]
+        { codigo: 1, descripcion: "coca-cola", precio: 2.5 },
+        { codigo: 2, descripcion: "inka-cola", precio: 2.2 },
+        { codigo: 3, descripcion: "fanta", precio: 1.7 },
+      ],
     };
     this.borrar = this.borrar.bind(this);
   }
@@ -19,44 +19,48 @@ class App extends Component {
       <div className="bg-dark text-light min-vh-100 d-flex flex-column justify-content-center align-items-center">
         <div className="container py-5">
           <h2 className="text-center mb-4">Lista de Artículos</h2>
-          <div className="table-responsive">
-            <table className="table table-striped table-hover table-bordered">
-              <thead className="thead-dark">
-                <tr className="text-center">
-                  <th>Código</th>
-                  <th>Descripción</th>
-                  <th>Precio</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.articulos.map(elemento => (
-                  <tr key={elemento.codigo} className="text-center">
-                    <td>{elemento.codigo}</td>
-                    <td>{elemento.descripcion}</td>
-                    <td>${elemento.precio.toFixed(2)}</td>
-                    <td>
-                      <button
-                        className="btn btn-danger btn-sm"
-                        onClick={() => this.borrar(elemento.codigo)}
-                      >
-                        Borrar
-                      </button>
-                    </td>
+          {this.state.articulos.length > 0 ? (
+            <div className="table-responsive">
+              <table className="table table-striped table-hover table-bordered">
+                <thead className="thead-dark">
+                  <tr className="text-center">
+                    <th>Código</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody>
+                  {this.state.articulos.map((elemento) => (
+                    <tr key={elemento.codigo} className="text-center">
+                      <td>{elemento.codigo}</td>
+                      <td>{elemento.descripcion}</td>
+                      <td>${elemento.precio.toFixed(2)}</td>
+                      <td>
+                        <button
+                          className="btn btn-danger btn-sm"
+                          onClick={() => this.borrar(elemento.codigo)}
+                        >
+                          Borrar
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <p className="text-center">No hay articulos disponibles</p>
+          )}
         </div>
       </div>
     );
   }
 
   borrar(cod) {
-    const temp = this.state.articulos.filter(el => el.codigo !== cod);
+    const temp = this.state.articulos.filter((el) => el.codigo !== cod);
     this.setState({
-      articulos: temp
+      articulos: temp,
     });
   }
 }
